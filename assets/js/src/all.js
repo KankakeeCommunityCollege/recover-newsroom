c0d2003bb14d82a045f9e69df28407c37c74ff5d
wdzajicek <tig.welder4@gmail.com>
2019-07-24 17:48:27 -0500

    Delete console.log statement.

--- assets/js/src/all.js
+++ assets/js/src/all.js
@@ -1,7 +1,6 @@
 import ieOnly from './ieOnly.js';
 import setDate from './date.js';
 import newsWidget from './newsWidget.js';
-import currentPage from './currentPage.js';
 import navbarScroll from './navbarScroll.js';
 import playModal from './playModal.js';
 import checkForPound from './jumpLink.js';
@@ -10,10 +9,8 @@ checkForPound();
 navbarScroll();
 
 document.addEventListener('DOMContentLoaded', function() {
-  console.log('HEY THERE');
   ieOnly();
   setDate();
   newsWidget();
-  currentPage();
   playModal();
 });
