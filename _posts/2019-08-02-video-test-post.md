6cb2dfde35ed4a46ec1356618f1829037ce8d605
wdzajicek <tig.welder4@gmail.com>
2019-08-02 10:25:39 -0500

    Fix Video post layout/functionality.

---
+++ _posts/2019-08-02-video-test-post.md
@@ -0,0 +1,24 @@
+---
+title: Microsoft Office Certificate available
+post_approved: true
+image:
+article_lead: Intro line to video post.
+video_link: https://youtu.be/U86YpREVWrw
+press_release: false
+---
+
+<br>![](/uploads/officecertificate1---copy.jpg){: .float-image-right width="351" height="351"}Kankakee Community College’s Harold and Jean Miner South Extension Center in Watseka is offering Microsoft Office Suite Certification training.&nbsp;
+
+The certificate program includes five eight-week classes. It can be completed in three semesters.
+
+The upcoming schedule is:
+
+* Word (COSC 1352) and PowerPoint (COSC 1341) -&nbsp; Fall 2019, beginning Oct. 14.&nbsp;
+* Access (COSC 1362) and Excel (COSC 1372)- Spring 2020 semester
+* Microsoft Office Suite Integration (COSC 1382)- Summer 2020 term
+
+For Fall 2019, registration must be completed before Sept. 6 for financial aid consideration.
+
+Upon completion, students may satisfy requirements for a particular position within the business microcomputer application field. The coursework also can be applied toward an Advanced Certificate in Office Assistant and an Associate in Applied Science Office Professional degree.
+
+For more information, phone or email Nancy Schunke at 815-802-8784 or [nschunke@kcc.edu](mailto:nschunke@kcc.edu); or stop by the KCC South Extension Center, 1488 E. Walnut St in Watseka.
--- assets/js/src/newsHeader.js
+++ assets/js/src/newsHeader.js
@@ -2,9 +2,9 @@ function initialAnimation() {
   const firstImage = document.querySelector('.news-header__img--first');
   const firstTitle = document.querySelector('.news-header__title--first');
   const firstLead = document.querySelector('.news-header__lead--first');
-  firstImage.classList.add('news-header__img--animate-in');
-  firstTitle.classList.add('news-header__title--animate-in');
-  firstLead.classList.add('news-header__lead--animate-in');
+  firstImage.classList.add('news-header__animate-in');
+  firstTitle.classList.add('news-header__animate-in');
+  firstLead.classList.add('news-header__animate-in');
 }
 
 function makePreviewActive(id) {
@@ -16,53 +16,40 @@ function makePreviewActive(id) {
 function animateImageIn(id) {
   let thisPreview = document.getElementById(id);
   let thisPreviewImage = thisPreview.querySelector('.news-header__img');
-  thisPreviewImage.classList.remove('news-header__img--animate-out');
-  thisPreviewImage.classList.add('news-header__img--animate-in');
+  thisPreviewImage.classList.remove('news-header__animate-out');
+  thisPreviewImage.classList.add('news-header__animate-in');
+}
+
+function animatePlayIn(id) {
+  let thisPreview = document.getElementById(id);
+  let thisPlayIcon = thisPreview.querySelector('.news-header__play-icon');
+  thisPlayIcon.classList.remove('news-header__animate-out');
+  thisPlayIcon.classList.add('news-header__animate-in');
 }
 
 function animateTitleIn(id) {
   let thisPreview = document.getElementById(id);
   let thisPreviewTitle = thisPreview.querySelector('.news-header__title');
-  thisPreviewTitle.classList.add('news-header__title--animate-in');
+  thisPreviewTitle.classList.add('news-header__animate-in');
 }
 
 function animateLeadIn(id) {
   let thisPreview = document.getElementById(id);
   let thisPreviewLead = thisPreview.querySelector('.news-header__lead');
-  thisPreviewLead.classList.add('news-header__lead--animate-in');
+  thisPreviewLead.classList.add('news-header__animate-in');
 }
 
-function animateLeadOut() {
-  let visiblePreviewLead = document.querySelectorAll('.news-header__lead--animate-in');
-  for (var i = 0; i < visiblePreviewLead.length; i++) {
-    let lead = visiblePreviewLead[i];
-    lead.classList.remove('news-header__lead--animate-in');
-    lead.classList.add('news-header__lead--animate-out');
+function animateItemsOut() {
+  let visiblePreviewItems = document.querySelectorAll('.news-header__animate-in');
+  for (var i = 0; i < visiblePreviewItems.length; i++) {
+    let item = visiblePreviewItems[i];
+    item.classList.remove('news-header__animate-in');
+    item.classList.add('news-header__img--animate-out');
   }
-}
-
-function animateTitleOut() {
-  let visiblePreviewTitle = document.querySelectorAll('.news-header__title--animate-in');
-  for (var i = 0; i < visiblePreviewTitle.length; i++) {
-    let title = visiblePreviewTitle[i];
-    title.classList.remove('news-header__title--animate-in');
-    title.classList.add('news-header__title--animate-out');
-  }
-  animateLeadOut();
-}
-
-function animateImageOut() {
-  let visiblePreviewImage = document.querySelectorAll('.news-header__img--animate-in');
-  for (var i = 0; i < visiblePreviewImage.length; i++) {
-    let image = visiblePreviewImage[i];
-    image.classList.remove('news-header__img--animate-in');
-    image.classList.add('news-header__img--animate-out');
-  }
-  animateTitleOut();
 }
 
 function removeActivePreview() {
-  animateImageOut();
+  animateItemsOut();
   const previews = document.querySelectorAll('.newsHeader-link-active');
   for (var i = 0; i < previews.length; i++) {
     let thisPreview = previews[i];
@@ -90,9 +77,13 @@ function newsHeader() {
 
     thisLink.addEventListener('mouseenter', function(e) {
       let previewTarget = e.target.dataset.preview;
+      const linkIsVideoPost = e.target.dataset.video !== undefined;
       removeActivePreview();
       makePreviewActive(previewTarget);
       animateImageIn(previewTarget);
+      linkIsVideoPost ?
+        animatePlayIn(previewTarget)
+      : null;
       animateTitleIn(previewTarget);
       animateLeadIn(previewTarget);
     });
