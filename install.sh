ac196168ad466e59ea01ba4c46b3683e188a5161
wdzajicek <tig.welder4@gmail.com>
2019-10-21 09:02:00 -0500

    Delete `Gemfile.txt` & update `install.sh`.

--- install.sh
+++ install.sh
@@ -1,27 +1,41 @@
+#!/bin/bash/
 
-echo "Creating local Gemfile from Gemfile.txt"
-
-sleep 1
-
-# Create local Gemfile from Gemfile.txt
-cp Gemfile.txt Gemfile
-
-echo "running npm install"
+echo "" && cowsay "Running npm install" && echo ""
 
 sleep .5
 
-echo "NPM will return permission errors if you do not have NVM - https://github.com/creationix/nvm"
+echo "" && echo "NPM will return permission errors if you do not have NVM - https://github.com/creationix/nvm" && echo ""
 
 sleep 1
 
 # NPM install
 npm install
 
-echo "running bundle install"
+echo "" && cowsay "Running bundle install" && echo ""
 
 sleep 1
 
 # Bundle install
 bundle install
 
-echo "Project Installation Complete!"
+sleep 1
+
+uname=$(uname)
+# If your on a Linux machine do...
+if [ "$uname" = "Linux" ]
+  then
+    # fix for node.js watch ENOSPC errors
+    # increases the max number of system watchers on linux
+    echo fs.inotify.max_user_watches=524288 | sudo tee -a /etc/sysctl.conf && sudo sysctl -p
+    echo "" && echo "[LINUX]: Increase max watchers on Linux platforms" && echo ""
+    sleep 1
+  else
+    echo "" && echo "[Skipping](optional): Increase max watchers on Linux platforms. Your system: $uname" && echo ""
+fi
+unset UNAME
+
+sleep 1
+
+echo "" && echo "======== DONE ========" && echo "" && echo "Installation Complete!" && echo "" && echo "======================"
+
+cowsay "That means finished!"
