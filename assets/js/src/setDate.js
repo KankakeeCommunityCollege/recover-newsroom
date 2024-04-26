0e49bf4181cc27bfdefde18e8976aa3bdf9fd693
wdzajicek <tig.welder4@gmail.com>
2024-04-26 09:33:24 -0500

    Build latest new date into page...
    * Use liquid to build today's date into the page and
      adjust JS to update it only if it's incorrect. This
      avoids a repaint from using `.innerHTML`; If the
      morning build/ftp errors-out the JS corrects it.
    * Darken the date's color to give more contrast with
      the background.

--- assets/js/src/setDate.js
+++ assets/js/src/setDate.js
@@ -3,9 +3,14 @@ const todaysDate = document.getElementById('todaysDate');
 
 function setDate() {
   const today = new Date();
-  const time = `${today.getMonth()+1}.${today.getDate()}`;
+  const formattedDate = `${today.getMonth() + 1}.${today.getDate()}`;
+  const dateBuiltIntoPage = todaysDate.innerText;
 
-  todaysDate.innerHTML = time;
+  // Most of the time the date built in the page will be correct as long as the morning build or FTP didn't error-out
+  if (dateBuiltIntoPage == formattedDate) // if the date is correct there is no need to update it
+    return;
+
+  todaysDate.innerHTML = formattedDate;
 }
 
 export default setDate;
